da0ff497b44430cc14e4da127c8af06b29a08933
abhiram <abhiramsibi2@gmail.com>
2026-07-08 12:07:24 -0400

    Added search page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import SearchPage from './components/SearchPage/SearchPage';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -20,6 +22,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/login" element={<LoginPage/>}/>
           <Route path="/app/register" element={<RegisterPage/>}/>
+         <Route path="/app/search" element={<SearchPage/>} />
 
         
         </Routes>
